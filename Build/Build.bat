5fddaec94bb37207d3e1bdb11d4729761704342c
jsmorley <jsmorley@gmail.com>
2016-03-18 16:11:41 -0400

    Updated build.bat with new url's for signtool timestamp. Updated version.* files with 3.3.1

--- Build/Build.bat
+++ Build/Build.bat
@@ -8,7 +8,7 @@ set GIT=%PROGRAMFILES%\Git\bin\git.exe
 :: Set VERSION_REVISION to non-zero value to override
 set VERSION_MAJOR=3
 set VERSION_MINOR=3
-set VERSION_SUBMINOR=0
+set VERSION_SUBMINOR=1
 set VERSION_REVISION=0
 set ISBETA=true
 
@@ -26,7 +26,11 @@ set MSBUILD="msbuild.exe" /nologo^
 	/p:Configuration=Release
 
 if exist "Certificate.bat" call "Certificate.bat" > nul
-set SIGNTOOL="signtool.exe" sign /t http://time.certum.pl /f "%CERTFILE%" /p "%CERTKEY%"
+:: http://time.certum.pl/
+:: http://timestamp.comodoca.com/authenticode
+:: http://timestamp.verisign.com/scripts/timestamp.dll
+:: http://timestamp.globalsign.com/scripts/timestamp.dll
+set SIGNTOOL="signtool.exe" sign /t http://timestamp.comodoca.com/authenticode /f "%CERTFILE%" /p "%CERTKEY%"
 
 if "%1" == "BUILDLANGUAGES" goto BUILDLANGUAGES
 
--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 3,3,0,2386
+#define FILEVER 3,3,1,2602
 #define PRODUCTVER FILEVER
-#define STRFILEVER "3.3.0.2386"
+#define STRFILEVER "3.3.1.2602"
 #define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"3.3.0"
-#define RAINMETER_VERSION ((3 * 1000000) + (3 * 1000) + 0)
-const int revision_number = 2386;
-const bool revision_beta = true;
+#define APPVERSION L"3.3.1"
+#define RAINMETER_VERSION ((3 * 1000000) + (3 * 1000) + 1)
+const int revision_number = 2602;
+const bool revision_beta = false;
